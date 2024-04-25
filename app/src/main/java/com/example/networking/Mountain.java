6b925d98b882d3e89a09f25cba83ef492a127161
d22marwi <d22marwi@student.his.se>
2024-04-25 16:05:06 +0200

    Implementerade TextView och Recycler_item.

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,7 +1,5 @@
 package com.example.networking;
 
-import androidx.annotation.NonNull;
-
 public class Mountain {
     private String name;
 
@@ -17,7 +15,7 @@ public class Mountain {
        this.name = name;
     }
 
-    @NonNull
+
     @Override
     public String toString(){
         return name;
