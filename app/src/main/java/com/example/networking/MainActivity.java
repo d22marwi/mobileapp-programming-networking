fd3f634692e6750d761850908b119d7004cd62c0
d22marwi <d22marwi@student.his.se>
2024-04-26 13:23:40 +0200

    Gjorde vissa förändrigar, lade till en adapter samt kollpade adapter till recyclerView.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.networking;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -37,5 +40,17 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         for (Mountain berg : ListOfMountains) {
             Log.d("Mountain", berg.toString());
             items.add(new Mountain(berg.getName()));
-        }}
+        }
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(Mountain item) {
+                Toast.makeText(MainActivity.this, item.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        RecyclerView recycler_view = findViewById(R.id.recycler_view);
+        recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        recycler_view.setAdapter(adapter);
+    }
 }
+
--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -4,20 +4,24 @@ public class Mountain {
     private String name;
 
     public Mountain(String name){
+
         this.name = name;
     }
 
     public String getName(){
+
         return name;
     }
 
     public void setName (String name){
-       this.name = name;
+
+        this.name = name;
     }
 
 
     @Override
     public String toString(){
+
         return name;
     }
 }
